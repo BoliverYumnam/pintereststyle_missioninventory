81682880cc7faa14278ef19b538b6554282c4d17
Global Santhi <yumnamboliver12389@gmail.com>
2025-09-28 14:54:12 +0530

    Second commit

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,18 +1,37 @@
+// Represents the links for a launch
+export interface LaunchLinks {
+  flickr?: { original: string[] };
+  wikipedia?: string;
+  article?: string;
+  patch?: {
+    small: string | null;
+    large: string | null;
+  };
+  webcast?: string | null;
+}
+
+// Represents a core for the launch
+export interface Core {
+  reused?: boolean;
+}
+
+// Represents a launch
 export interface Launch {
   id: string;
   name: string;
   date_utc: string;
   success: boolean | null;
   details: string | null;
-  links: {
-    patch: {
-      small: string | null;
-      large: string | null;
-    };
-    webcast: string | null;
-  };
+  
+  flight_number?: number;
+  upcoming?: boolean;
+  cores?: Core[];
+  rocket?: string;
+  launchpad?: string;
+  payloads?: string[];
+  
+  links?: LaunchLinks;
 }
-
 
 // Represents each entry returned by the Public APIs API
 export interface ApiEntry {
